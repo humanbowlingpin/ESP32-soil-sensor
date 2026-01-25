f0d9c2dc0975a6676cb570a6b0b22e20741054e3
humanbowlingpin <161883455+humanbowlingpin@users.noreply.github.com>
2026-01-25 21:50:40 +0700

    thai (unfinished)

--- website.h
+++ website.h
@@ -152,10 +152,10 @@ const char index_html[] PROGMEM = R"rawliteral(
             <div class="card"><span class="label">Humidity</span> : <span class="value">%HUMID% &percnt;</span></div>
             <div class="card"><span class="label">Temp</span> : <span class="value">%TEMP% °C</span></div>
             <div class="card"><span class="label">pH</span> : <span class="value">%PH%</span></div>
-            // <div class="card"><span class="label">EC</span> : <span class="value">%EC%</span></div>
             <div class="card"><span class="label">Nitrogen (N)</span> : <span class="value">%N%</span></div>
             <div class="card"><span class="label">Phosphorus (P)</span> : <span class="value">%P%</span></div>
             <div class="card"><span class="label">Potassium (K)</span> : <span class="value">%K%</span></div>
+            <div class="card"><span class="label">EC</span> : <span class="value">%EC%</span></div>
         </div>
 
         <div class="recommend-box">
